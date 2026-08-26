f740dbcfadbd08b784b0f5d910df7756d29c2f46
lucaspoupee-oss <lucaspoupee@gmail.com>
2026-08-26 13:51:25 +0200

    Add files via upload

---
+++ src/player/Player.jsx
@@ -0,0 +1,18 @@
+import React, { memo } from "react";
+import ReactPlayer from "react-player";
+
+export function Player({streamLinkId, url, playing, loop, volume, muted, onEnded}) {
+	return (
+		<ReactPlayer
+			key={streamLinkId}
+			url={url}
+			playing={playing}
+			loop={loop}
+			volume={volume}
+			muted={muted}
+			onEnded={onEnded}
+		/>
+	);
+}
+
+export const MemoizedPlayer = memo(Player);
